75355aef475cbe933aadeb5af05a5b36e10fc24e
EQQX2 <beboreport1@gmail.com>
2026-08-25 21:18:01 +0100

    v2: bilingual, prayer times, goals, seasons, quran, gym, PWA

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE='fp-v2';
+var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS).catch(function(){})}));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}));
+  }).then(function(){return self.clients.claim()}));
+});
+self.addEventListener('fetch',function(e){
+  var u=e.request.url;
+  if(e.request.method!=='GET')return;
+  if(u.indexOf('supabase.co')>=0||u.indexOf('aladhan.com')>=0)return;
+  e.respondWith(
+    fetch(e.request).then(function(r){
+      var copy=r.clone();
+      caches.open(CACHE).then(function(c){c.put(e.request,copy).catch(function(){})});
+      return r;
+    }).catch(function(){return caches.match(e.request).then(function(m){return m||caches.match('./index.html')})})
+  );
+});
