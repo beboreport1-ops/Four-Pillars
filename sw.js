18344b3b8e33a942feaee40d3a0a1d7b8d640429
EQQX2 <beboreport1@gmail.com>
2026-08-25 22:12:39 +0100

    Top-level nav: Main / Schedule / Stats / Studying / Health / Social / Soul

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v7';
+var CACHE='fp-v8';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
