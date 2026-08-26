55c7ac87dd4923431944f24898a292dba73ab88b
EQQX2 <beboreport1@gmail.com>
2026-08-26 08:21:24 +0100

    Attendance % measured against full semester, not classes held

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v10';
+var CACHE='fp-v11';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
