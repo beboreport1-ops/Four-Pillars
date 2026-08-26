9c54f12341a4bf83c6d0d31606395f58961d34a1
EQQX2 <beboreport1@gmail.com>
2026-08-26 16:19:18 +0100

    Rebuild trigger

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v18';
+var CACHE='fp-v19';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
