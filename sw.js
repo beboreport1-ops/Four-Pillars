bc2a26777a08e19b90ea1025c92cca353ef3db57
EQQX2 <beboreport1@gmail.com>
2026-08-26 08:42:22 +0100

    Real timetable grid; calendar only on Schedule tab

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v13';
+var CACHE='fp-v14';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
