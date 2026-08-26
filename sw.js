b325a4fbdfc2bd08659e519169da2b4012800f27
EQQX2 <beboreport1@gmail.com>
2026-08-26 22:20:53 +0100

    Cleanup: fix shadowed semWeekNum bug, remove dead code, CSS, i18n keys

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v23';
+var CACHE='fp-v24';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
