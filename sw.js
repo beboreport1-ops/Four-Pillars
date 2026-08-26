584766bd4a7a935b001cf584b334715dd5f557f4
EQQX2 <beboreport1@gmail.com>
2026-08-26 22:35:51 +0100

    Simplify grade card to running marks out of 100

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v26';
+var CACHE='fp-v27';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
