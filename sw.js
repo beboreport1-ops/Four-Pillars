dcba900ff092476f7378bc3744a38f30f653bd4e
EQQX2 <beboreport1@gmail.com>
2026-08-26 22:32:00 +0100

    Grades: each course is a running score out of 100

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v25';
+var CACHE='fp-v26';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
