89ea3b040ef7d7ca531605c2798b136bc1e0717c
EQQX2 <beboreport1@gmail.com>
2026-08-26 08:30:20 +0100

    Academic calendar: breaks, holidays and finals excluded from attendance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v11';
+var CACHE='fp-v12';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
