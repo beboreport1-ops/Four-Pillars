51ab25142f771db6de12f2c105cd3279214f3e29
EQQX2 <beboreport1@gmail.com>
2026-08-26 21:51:09 +0100

    Course pages: deadlines, exams, quizzes, scores and rooms

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v22';
+var CACHE='fp-v23';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
