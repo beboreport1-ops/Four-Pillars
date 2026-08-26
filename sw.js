debc74421da0da67b3ff302842bbc8b019dd89d9
EQQX2 <beboreport1@gmail.com>
2026-08-26 22:26:50 +0100

    Simplify event form: no room, optional time, single marks field

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v24';
+var CACHE='fp-v25';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
