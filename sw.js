66de3a60dd5a1d7bbe26757bb95ebe48d298ed89
EQQX2 <beboreport1@gmail.com>
2026-08-26 16:10:08 +0100

    My Courses shows absence % against the 25% limit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v17';
+var CACHE='fp-v18';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
