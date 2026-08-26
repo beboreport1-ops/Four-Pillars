b2307d2455764f153cd0c9d9aaf3b835f4160739
EQQX2 <beboreport1@gmail.com>
2026-08-26 22:40:43 +0100

    Log steps when logging a Walk

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v27';
+var CACHE='fp-v28';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
