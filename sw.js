923344d85112b71cde995feaae27b440a3327833
EQQX2 <beboreport1@gmail.com>
2026-08-28 23:30:41 +0100

    Add leg day exercises

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v31';
+var CACHE='fp-v32';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
