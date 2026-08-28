190a2b4a50b66e2f599e8dd54ba0ab8e053f9223
EQQX2 <beboreport1@gmail.com>
2026-08-28 23:28:55 +0100

    Push: swap shoulder press for lateral raise; add pull day exercises

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v30';
+var CACHE='fp-v31';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
