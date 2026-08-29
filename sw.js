bdaaeda88bac88405ad54044e7dbcca5f382a65b
EQQX2 <beboreport1@gmail.com>
2026-08-29 04:53:10 +0100

    Spacing: safe-area top for header, gap between header and content

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v36';
+var CACHE='fp-v37';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
