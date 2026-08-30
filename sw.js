d514df72a622dd9dd05616588b936dd99aa353aa
EQQX2 <beboreport1@gmail.com>
2026-08-30 01:38:30 +0100

    Focus timer (drift-proof, survives backgrounding); remove duplicate modal backdrop listener (fp-v43)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v42';
+var CACHE='fp-v43';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
