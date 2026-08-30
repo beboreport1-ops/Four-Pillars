ebe9abba1b1324a42931a2613ddb003d88e5c24b
EQQX2 <beboreport1@gmail.com>
2026-08-30 01:16:58 +0100

    Batch 2: modal Escape close, focus trap + restore, backdrop dismiss for confirms, 44px icon-button tap targets (fp-v39)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v38';
+var CACHE='fp-v39';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
