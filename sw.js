f2aae61b5c0ad31f43cccc3bf538310f15cbd729
EQQX2 <beboreport1@gmail.com>
2026-08-30 01:36:18 +0100

    Deadline strip on Main, ICS timetable export, canvas font fix, final-kind icon (fp-v42)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v41';
+var CACHE='fp-v42';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
