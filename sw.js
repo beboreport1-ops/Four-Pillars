6065490adb7751268d9f9d86b40a73831a18c199
EQQX2 <beboreport1@gmail.com>
2026-08-31 11:03:58 +0100

    v47: highlight current or next class in today view

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v46';
+var CACHE='fp-v47';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
