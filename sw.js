53a9b724726b65736760688f844db527352150dc
EQQX2 <beboreport1@gmail.com>
2026-08-31 23:13:46 +0100

    v50: stat grid, FAB collision, tab strip fade, haptics, empty grade card, hrs unit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v49';
+var CACHE='fp-v50';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./404.html','./og.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
