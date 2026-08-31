5bc8277c8b0bd805cbf6d432ec75d0ae3b52c7c8
EQQX2 <beboreport1@gmail.com>
2026-08-31 10:50:30 +0100

    v45: room number in class rows, tap class to open course

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v44';
+var CACHE='fp-v45';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
