2ad0c0c27f1c26e485d92dedbb87a6ca7660b803
EQQX2 <beboreport1@gmail.com>
2026-09-01 09:30:19 +0100

    v54: Tier 2 cleanup - courseOf/entryOf helpers, ACCGRP hoist, drop wempty, define .frow, prayer-cache sweep

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v53';
+var CACHE='fp-v54';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./404.html','./og.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
