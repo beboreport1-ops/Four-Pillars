5e573668d5e6ba7c866f460588eb4babed88828e
EQQX2 <beboreport1@gmail.com>
2026-09-01 08:52:24 +0100

    v52: weights editor creates grade items with no date instead of studyEnd()

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v51';
+var CACHE='fp-v52';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./404.html','./og.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
