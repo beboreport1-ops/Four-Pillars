7073ef70603c69faea646d71a0dad642c1eb3531
EQQX2 <beboreport1@gmail.com>
2026-09-03 23:23:17 +0300

    v59: glass surface tiers with ghost stroke, pillar-tracking hero bloom, live-class gold bloom

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fp-v58';
+var CACHE='fp-v59';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./404.html','./og.png','./inter.woff2','./jakarta.woff2'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
